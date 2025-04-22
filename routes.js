7964f9cef6e02fb8abf51e989444866a27083334
lucasf-souza <lucasglsd2424@gmail.com>
2025-04-22 18:33:11 -0300

    Adiciona funcionalidade de contato e atualiza navegação

--- routes.js
+++ routes.js
@@ -3,12 +3,19 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 route.get('/', homeController.index);
-route.get('/login', loginController.index);
 
+route.get('/login', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login',    loginController.login);
 route.get('/logout', loginController.logout);
+
+route.get('/contato', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.index);
+
 
 module.exports = route;
---
+++ src/controllers/contatoController.js
@@ -0,0 +1,28 @@
+const contato = require ('../models/ContatoModel')
+
+exports.index = (req, res) => {
+    res.render('contato')
+}   
+exports.register = async (req, res) => {
+    try {
+      const contato = new Contato(req.body);
+      await contato.register();
+  
+      // Exibe erros no console
+      if (contato.errors.length > 0) {
+        console.log('Erros de validação:', contato.errors); 
+        req.flash('errors', contato.errors);
+        req.session.save(() => res.redirect('back'));
+        return;
+      }
+  
+      req.flash('success', 'Contato registrado com sucesso.');
+      req.session.save(() => res.redirect('back'));
+      return;
+  
+    } catch (e) {
+      console.log(e);
+      return res.render('404');
+    }
+  };
+  

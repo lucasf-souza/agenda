7a25280edfd2187837f65cbde93eb662d1a1fd54
lucasf-souza <lucasglsd2424@gmail.com>
2025-04-24 20:29:12 -0300

    Atualização de contato.

--- routes.js
+++ routes.js
@@ -17,6 +17,6 @@ route.get('/logout', loginController.logout);
 route.get('/contato', loginRequired, contatoController.index);
 route.post('/contato/register', loginRequired, contatoController.register);
 route.get('/contato/:id', loginRequired, contatoController.editIndex);
-
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
 
 module.exports = route;
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -17,6 +17,7 @@ exports.register = async (req, res) => {
 
     req.flash('success', 'Contato registrado com sucesso.');
     req.session.save(() => res.redirect(`/contato/${contato.contato._id}`));
+    return;
   } catch (e) {
     console.log(e);
     res.render('404');
@@ -31,3 +32,26 @@ exports.editIndex = async (req, res) => {
 
   res.render('contato', { contato });
 };
+
+exports.edit = async (req,res) => {
+  try{
+    if (!req.params.id) return res.render('404');
+    const contato = new Contato (req.body)
+    await contato.edit(req.params.id)
+
+    
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('/contato'));
+      return;
+    }
+
+    req.flash('success', 'Contato Atualizado com sucesso.');
+    req.session.save(() => res.redirect(`/contato/${contato.contato._id}`));
+    return;
+  }catch(e) {
+    console.log(e)
+    res.render('404')
+  }
+ 
+}
